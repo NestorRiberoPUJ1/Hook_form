aafa16962761d28c5ba282c7ef37c0cf4d073632
Nestor Eduardo Ribero Vargas <nestor_05ribero@hotmail.com>
2022-04-28 18:00:35 -0500

    Update Formulario.js

--- hook-form/src/components/Formulario/Formulario.js
+++ hook-form/src/components/Formulario/Formulario.js
@@ -3,36 +3,105 @@ import React, { useState } from "react";
 const Formulario = () => {
 
     const [firstName, setFirstName] = useState("");
+    const [firstNameError, setFirstNameError] = useState("");
+
     const [lastName, setLastName] = useState("");
+    const [lastNameError, setLastNameError] = useState("");
+
     const [email, setEmail] = useState("");
+    const [emailError, setEmailError] = useState("");
+
     const [password, setPassword] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+
     const [confirmPassword, setConfirmPassword] = useState("");
+    const [confirmPasswordError, setConfirmPasswordError] = useState("");
+
+    const errorName = (e) => {
+        setFirstName(e.target.value);
+        if (e.target.value.length > 0 && e.target.value.length < 2) {
+            setFirstNameError("First name must be at least 2 characters");
+
+        }
+        else {
+            setFirstNameError("");
+        }
+    }
+
+    const errorLastName = (e) => {
+        setLastName(e.target.value);
+        if (e.target.value.length > 0 && e.target.value.length < 2) {
+            setLastNameError("Last name must be at least 2 characters");
+
+        }
+        else {
+            setLastNameError("");
+        }
+    }
+
+    const errorEmail = (e) => {
+        setEmail(e.target.value);
+        if (e.target.value.length > 0 && e.target.value.length < 5) {
+            setEmailError("Email must be at least 5 characters");
+
+        }
+        else {
+            setEmailError("");
+        }
+    }
+
+    const errorPassword = (e) => {
+        setPassword(e.target.value);
+        if (e.target.value.length > 0 && e.target.value.length < 8) {
+            setPasswordError("Password must be at least 8 characters");
+
+        }
+        else {
+            setPasswordError("");
+        }
+    }
+
+    const errorConfirmPassword = (e) => {
+        setConfirmPassword(e.target.value);
+        if (e.target.value !== password && e.target.value.length > 0) {
+            setConfirmPasswordError("Passwords do not match");
+
+        }
+        else {
+            setConfirmPasswordError("");
+        }
+    }
 
     return (
 
         <div className="container">
 
-            <form onSubmit={""}>
+            <form>
 
                 <div className="form-control">
                     <label>First Name:</label>
-                    <input className="form-control" onChange={(e) => setFirstName(e.target.value)} value={firstName} />
+                    <input className="form-control" onChange={(e) => errorName(e)} value={firstName} />
+                    <p>{firstNameError}</p>
                 </div>
                 <div className="form-control">
                     <label>Last Name:</label>
-                    <input className="form-control" onChange={(e) => setLastName(e.target.value)} value={lastName} />
+                    <input className="form-control" onChange={(e) => errorLastName(e)} value={lastName} />
+                    <p>{lastNameError}</p>
                 </div>
                 <div className="form-control">
                     <label>Email:</label>
-                    <input className="form-control" onChange={(e) => setEmail(e.target.value)} value={email} />
+                    <input className="form-control" onChange={(e) => errorEmail(e)} value={email} />
+                    <p>{emailError}</p>
                 </div>
                 <div className="form-control">
                     <label>Password:</label>
-                    <input type="password" className="form-control" onChange={(e) => setPassword(e.target.value)} value={password} />
+                    <input type="password" className="form-control" onChange={(e) => errorPassword(e)} value={password} />
+                    <p>{passwordError}</p>
                 </div>
                 <div className="form-control">
                     <label>Password Confirm:</label>
-                    <input type="password" className="form-control" onChange={(e) => setConfirmPassword(e.target.value)} value={confirmPassword} />
+                    <input type="password" className="form-control" onChange={(e) => errorConfirmPassword(e)} value={confirmPassword} />
+                    <p>{confirmPasswordError}</p>
                 </div>
 
             </form>
